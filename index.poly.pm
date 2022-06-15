7aa341d464e81cd1d31957a36c425533d64cd6a8
haglobah <83699832+haglobah@users.noreply.github.com>
2022-06-15 20:27:07 +0200

    Fix typos

--- index.poly.pm
+++ index.poly.pm
@@ -11,21 +11,21 @@ HERE
 ◊sec["Whom is this guide for?" 1]{
 	For anyone who wants to learn more while doing the 42 Curriculum. 
 	This book aims to take some stones out of your way while traversing the Curriculum, while also providing some kind of general map for the Terrain. 
-	Note, however, that it aims to be a benevolent Guide for you own long-term learning. It might discuss stuff you do not need right know to solve the task at hand. Rather, it tries to show you what other ways you might walk in order to achieve that. 
+	Note, however, that it aims to be a benevolent Guide for your own long-term learning. It might discuss stuff you do not need right know to solve the task at hand. Instead, it tries to show you what other ways you might walk in order to achieve that. 
 }
 
 ◊sec["Why?" 1]{
-	Because the Curriculum right now is putting stones right in front of you. I am not sure whether this happened by accident or on purpose, and it should force you to ask you peers. What I am sure about, however, is that it hinders learning and slows you down. This time with your peers surely is better spent on discussing different approachs of how to solve a Project, rather than on figuring out what the hell the projects want you to do. 
+	Because the Curriculum right now is putting stones right in front of you. I am not sure whether this happened by accident or on purpose (and it should 'encourage' you to ask your peers). What I am sure about, however, is that it hinders learning and slows you down. This time with your peers surely is better spent on discussing different approaches on how to solve a project, rather than on figuring out what the hell the projects want you to do. 
 }
 
 ◊sec["Purpose & Goals" 1]{
 	This book is written for the ones who want to learn more than needed to complete the projects.
 	We'd like to provide:
 	◊ul{
-		◊li{A Map of the terrain next to each project, and from one to another.}
-		◊li{A good mindset to learn stuff.}
+		◊li{A map of the terrain around to each project, and of the paths from one to another.}
+		◊li{A good mindset to learn things.}
 		◊li{A comprehensive additional resource for each project.}
-		◊li{General advice on doing stuff at 42, programming-related and non-programming-related.}
+		◊li{General advice, thoughts and ideas on doing stuff at 42, programming-related and non-programming-related.}
 	}
 }
 
@@ -43,4 +43,3 @@ HERE
 ◊ul{
     ◊;li{◊link["cs_theory.html"]{Compound Data Types}}
 }
-◊;(pagetree-code)
