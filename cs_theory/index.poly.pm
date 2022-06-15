0681e4ecce063668ca3e2398983543a9492ad67e
unknown <hagenlob@posteo.de>
2022-06-15 17:20:08 +0200

    Add short Intros for core & cs_theory

--- cs_theory/index.poly.pm
+++ cs_theory/index.poly.pm
@@ -1,3 +1,7 @@
 #lang pollen
 
-◊title{CS Theory}
+◊title[#:sub "A collection of explanations"]{Computer Science Theory}
+
+◊narr{You will encounter CS concepts throughout the Curriculum – this is were all their explainers live.
+
+Enjoy :)}
