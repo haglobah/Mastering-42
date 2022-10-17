7e793ca35aa198fee6d7e2b357d1c17d5da37e9a
haglobah <bah@posteo.de>
2022-10-17 17:34:28 +0200

    cs_theory -> explainers

--- index.poly.pm
+++ index.poly.pm
@@ -48,8 +48,8 @@ HERE
     ◊li{◊link["core/vscode_debugger.html"]{Using the VS Code Debugger}}
 }
 
-◊heading[2]{◊link["cs_theory/index.html"]{Computer Science Theory}}
+◊heading[2]{◊link["explainers/index.html"]{Computer Science Theory}}
 
 ◊ul{
-    ◊;li{◊link["cs_theory.html"]{Compound Data Types}}
+    ◊;li{◊link["explainers.html"]{Compound Data Types}}
 }
