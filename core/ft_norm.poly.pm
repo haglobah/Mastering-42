cf010f65459ba6fa430ba2d6ce51023e8fdd4c42
haglobah <bah@posteo.de>
2022-11-28 16:04:59 +0100

    Add Fighting the Norm

--- core/ft_norm.poly.pm
+++ core/ft_norm.poly.pm
@@ -1,44 +1,148 @@
 #lang pollen
 
-◊title[#:sub "by bhagenlo"]{Fighting the Norm}
+◊title[#:sub "by bhagenlo"]{Fighting the Norm (and C)}
 
-If you're like me, and haven't programmed extensively in C before, you might know the struggle.
+◊narr{If you're like me, and haven't programmed extensively in C before, you might know the struggle.
+
+You know abstractly what to do, somehow manage to do it in C, but then, ◊c{norminette} interferes.}
+
+This is for those who are fighting those fights, too.
+
+◊h[1]{Oh no: my function's too long}
+
+For shorter loops & ifs, consult: ◊l["https://github.com/agavrel/42_CheatSheet#0x05--swindle-the-norminette---truander-la-norme"]{this guide}. 
+
+◊h[2]{Hacking together the ternary operator}
+
+◊code-block['c #:nums? #f]{
+	a = (isneg ? -num : num)
+}
+
+becomes
+
+◊code-block['c #:nums? #f]{
+	a = num * (-2 * isneg + 1)
+}
+
+– which is totally legal.
 
 
+◊h[1]{Oh no: I'm declaring too many variables}
 
+◊h[2]{Using an array}
 
+Well, for that — provided the arguments are of the same length – you can just use an array.
 
+This way, 
+◊code-block['c #:filename "myfun.c"]{
 
-
-
-
-◊h[2]{Option 1: Using ◊c{system()}}
-
-There is already a command on our system to find memory leaks: ◊l["https://www.manpagez.com/man/1/leaks/"]{◊c{leaks}}
-
-With that, the only thing left is to call it:
-
-◊code-block['c]{
-
-	#include <stdlib.h>
-	...
-	int main()
+	void	myfun(void)
 	{
-		...
-		system("leaks a.out");
-		return (0);
+		int	i;
+		int	j;
+		int	wordcount;
+		int	charcount;
+		int word_started;
+		int	x;
+		int y;
 	}
 }
 
-That will print out your leaks.
+becomes
 
-◊h[2]{Option 2: Using the LeakSanitizer}
+◊code-block['c #:filename "myfun.c"]{
 
-A little more sophisticated method, as well as one you don't have to change your code for.
+	void	myfun(void)
+	{
+		int vars[7];
 
-It has a nice & short guide on its ◊l["https://github.com/mhahnFr/LeakSanitizer"]{repo}.
-I, for my part, added it to my ◊l["./makefiles.html#Using%20the%20LeakSanitizer"]{◊c{Makefile}}.
+		vars = populate_vars();
+	}
+}
 
-◊h[2]{Option 3: Using Valgrind}
+But, in my opinion, there's a better option.
 
-Much more sophistication. A short guide ◊l["https://valgrind.org/docs/manual/quick-start.html"]{here}.
+◊h[2]{Usign structs}
+
+You can just declare a struct.
+
+◊code-block['c #:filename "myproject.h"]{
+
+	typedef struct s_myfun_struct
+	{
+		int	i;
+		int	j;
+		int	wordcount;
+		int	charcount;
+		int word_started;
+		int	x;
+		int y;
+	}	t_mf;
+}
+
+With that, ◊c{myfun} becomes:
+
+◊code-block['c #:filename "myfun.c"]{
+
+	void	myfun(void)
+	{
+		t_mf	mf_struct;
+
+		mf_struct = (t_mf){.i = 0, .j = 0,
+						   .wordcount = 0, .charcount = 0, .word_started = 0,
+						   .x = 0, .y = 0};
+	}
+}
+
+Feel free to omit the member names:
+◊code-block['c]{
+		mf_struct = (t_mf){0, 0, 0, 0, 0, 0, 0};
+}
+
+Or, if you really want to initialize every member to zero, you can even use:
+◊code-block['c]{
+		mf_struct = (t_mf){};
+}
+
+◊heading[1]{Oh no: I am using too many arguments to my function}
+
+Another case for structs.
+
+◊code-block['c #:filename "utils.c"]{
+
+	void	super_general_fun(int a, int b, char *s1, char *s2, bool is_valid, bool is_upper, int *n)
+	{
+		...
+	}
+}
+
+becomes
+
+◊code-block['c #:filename "myproject.h"]{
+
+	typedef struct s_super_general_struct
+	{
+		int		a;
+		int		b;
+		char	*s1;
+		char	*s2;
+		bool	is_valid;
+		bool	is_upper;
+		int		*n;
+	}	t_sg;
+}
+
+◊code-block['c #:filename "utils.c"]{
+
+	void	super_general_fun(t_sg s)
+	{
+		...
+	}
+}
+
+◊sec["Pointers" 1]{
+
+	◊ul{
+		◊li{The ◊l["https://github.com/agavrel/42_CheatSheet#0x05--swindle-the-norminette---truander-la-norme"]{Swindle the norminette} section of the 42 Cheat Sheet.}
+	}
+}
