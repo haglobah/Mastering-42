7258a181689918eb2e3f7be5b0242cc568c3e56b
Beat Hagenlocher <bah@posteo.de>
2022-12-06 17:16:19 +0100

    Create exams.poly.pm

---
+++ core/exams.poly.pm
@@ -0,0 +1,25 @@
+#lang pollen
+
+◊title[#:sub "by bhagenlo"]{Writing Exams}
+
+◊narr{Hi!
+
+Ready for your first (or next) exam? Excited yet? Here's what you need to know (so that you don't waste time during one.)
+}
+
+In general, (at least at 42Heilbronn), in an Exam you have to do the following:
+
+(Don't worry, it's a explained with an overlay over the wallpaper, too – it's just that nervous people make mistakes :))
+
+◊ol{
+	◊li{Log in onto the system.
+		Username: ◊c{exam}
+		Password: ◊c{exam}}
+	◊li{Start up a shell. Execute ◊c{kinit <intraname>}
+		With that, you should be up and running}
+	◊li{}
+}
+
+◊sec["Exam 02" 1]{
+	
+}
