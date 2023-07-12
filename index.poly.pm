c618c5006373fb351b5fe38d840616c386547451
Beat Hagenlocher <bah@posteo.de>
2023-07-12 18:52:08 +0200

    Add ft_irc guide

--- index.poly.pm
+++ index.poly.pm
@@ -48,6 +48,7 @@ HERE
     ◊li{◊link["holy_graph/netpractice.html"]{NetPractice}}
     ◊li{◊link["holy_graph/miniRT.html"]{miniRT}}
     ◊li{◊link["holy_graph/cpp00-04.html"]{CPP00-04}}
+    ◊li{◊link["holy_graph/ft_irc.html"]{ft_irc}}
     ◊li{◊link["holy_graph/ft_containers.html"]{ft_containers}}
 }
 
