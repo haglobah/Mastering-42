f202d4b5da16fdefc7d975611e7d73da04708b3a
Beat Hagenlocher <bah@posteo.de>
2023-07-12 18:52:08 +0200

    Add ft_irc guide

--- holy_graph/ft_irc.poly.pm
+++ holy_graph/ft_irc.poly.pm
@@ -1,83 +1,93 @@
 #lang pollen
 
-◊(require "../templates.rkt")
-◊(define-meta level "5")
+◊title[#:sub "by bhagenlo" #:version "7.1"]{ft_irc}
 
-◊title[#:sub "by marv�u��z�" #:version "NULL"]{ft_irc}
-
-◊(insert-intro (select 'level metas))
-
-◊;{
-
-For just playing around, please follow [Getting up and running](https://github.com/haglobah/Mastering-42#getting-up-and-running).
-
-To actually change something on the website, please first read our [Contributing](https://github.com/haglobah/Mastering-42#contributing) guideline.
-
-Then, add/change what you want to add/change. Below, you find a template and some common code examples.
-
-For writing new content, please try to stick with our [style guide](https://github.com/haglobah/Mastering-42/blob/main/README.md#style-guide).
-
-Make sure _you commit your stuff to a new branch_.
-
-After you're done, don't forget to:
-- Add your name to the list of collaborators.
-- Update the version number to the one that's in the subject (replacing NULL)
-- In addition to that:
-  - If you added a section, reduce the level of ◊(define-meta level _) by one.
-  - If you completed the guide (it has no sections that are commented out anymore), remove this section, ◊(insert-intro ...) and lines 3 & 4, as well as marv�u��z� from the list of collaborators.
-
-Looking forward to your pull request!
-
+◊narr{
+	
+	Wow. You've come a long way. This is your final low-level project of the Core.
+	
+	Time to apply all the software engineering skills you've acquired so far. Time to shine.
 }
 
-◊;{
+But that does not mean that this is the place to use every feature, hack, or technique you know about. Software engineering is about using the right tools & structure, not about using all of them.
+
+So, what would make sense?
+How could/should ft_irc be structured?
+How should one work on such a big project in general?
+
 ◊sec["Prerequisites" 1]{
 
-A
-◊ul{
-	◊li{list}
-	◊li{of}
-	◊li{prerequisites.}
-}
+	There are two areas of attack:
 
-◊hint[#:type "warning"]{
-	Be very careful to check that!
-}
-
+	◊li-mark{1.} Unix Sockets
+	◊ul{
+		◊li{Understand what a Unix Socket is.}
+		◊li{Understand how ◊c{poll()}/◊c{select()} work.}
+		◊li{Implement an ◊l["https://protohackers.com/problem/0"]{Echo Server} that can handle concurrent connections with ◊c{poll()} or ◊c{select()}. (You can open them up by using netcat (◊c{nc}), which you need to test your ◊c{ft_irc} anyway.)
+		This should cost you only very little time, will clear up things, and is good practice for ◊c{mini_serv}.}
+	}
+	◊li-mark{2.} The IRC protocol & ecosystem 
+	◊ul{
+		◊li{Get familiar with the documentation of IRC. There are the RFCs (◊l["https://datatracker.ietf.org/doc/html/rfc1459"]{RFC1459}, ◊l["https://datatracker.ietf.org/doc/html/rfc2810"]{RFC2810}, ◊l["https://datatracker.ietf.org/doc/html/rfc2811"]{RFC2811}, ◊l["https://datatracker.ietf.org/doc/html/rfc2812"]{RFC2812}, ◊l["https://datatracker.ietf.org/doc/html/rfc2813"]{RFC2813}), as well as an attempt to summarize what everyone actually uses: ◊l["https://modern.ircdocs.horse/"]{modern.ircdocs.horse}}
+		◊li{Try out a few IRC clients, and actually use them for a short time. This should give you some intuition on how the protocol works, really.}
+		◊li{◊e{Choose your IRC client.} This may sound like it's way too early, but at least our experience they can be quite different in behaviour. So pick one, test with it from the beginning, and stick with it.}
+	}
 }
 
 ◊sec["During" 1]{
 
-	◊spoiler{I try not to steal the fun of thinking for oneself.} Shh. ◊spoiler{Don't let me be seen by accident.}
+	◊ol{
+		◊li{Did you choose your IRC client?}
+		◊li{Did you implement an Echo Server?}
+	}
+	Well then, you're ready to go.
 
-	◊sec["A subsection" #:sub "only sometimes important." 2 #:open? #f]{
-		Only for those who want to do the bonus.
-}
+	The most important thing up front: ◊e{Testing is key}.
+	We lost almost a week for 'just going with the protocol' when the client we chose (KVIrc) was expecting more than we thought it needed.
+
+	So: Get to a working state (meaning: You have a server you can connect to, and that answers your requests) as soon as possible. This should be quite easy, especially since you already wrote your Echo Server, didn't you? ^^
+
+	Then, implement a good logging infrastructure. Print out every command the server receives and sends, plus everything else that might be useful.
+	
+	From then on, build feature by feature incrementally, always ensuring that the new feature works, and old features don't break along the way.
+
+	For building those, you might want to get a log (from other teams, maybe?) of what your client actually expects.
+
+	◊; -> something about the general object-tree structure?
 }
 
 ◊sec["Cleaning Up" 1]{
 
-An
-◊ol{
-	◊li{ordered}
-	◊li{list}
-	◊li{of}
-	◊li{clean-up steps.}
-}
-
+	There's one thing in the subject that is easy to forget until the very end: 
+	◊il{
+		◊li{
+			The proper handling of concurrent connections with partially sent messages. Make sure your server can handle this as specified in the subject.
+		}
+	}
 }
 
 ◊sec["Aftercare" 1]{
 
+	And that was already it? Well done!
 
-}
+	And, strangely, already the last project of the Core you have to do in C++.
+
+	How was C++? Evaluate it according to the criteria outlined ◊l["./miniRT.html#Aftercare"]{here}. After this, you probably want to start with the preparation for your ◊c{mini_serv} exam. Good luck!
 }
 
 ◊narr{
-In case I do know other places where you'll might find what you need, I'll let you know ◊l["#Pointers"]{below}. Good luck with your search!
+
+	What a journey. And you came so far!
+	Only one big coding project left. I wish you the best.
+
+	And may your coworkers always write understandable code.
 }
 
 ◊sec["Pointers" 1]{
 
-
+	◊ul{
+		◊li{◊l["https://ircgod.com/"]{An old (but still useful guide for IRC)}}
+		◊li{The RFCs: ◊l["https://datatracker.ietf.org/doc/html/rfc1459"]{RFC1459}, ◊l["https://datatracker.ietf.org/doc/html/rfc2810"]{RFC2810}, ◊l["https://datatracker.ietf.org/doc/html/rfc2811"]{RFC2811}, ◊l["https://datatracker.ietf.org/doc/html/rfc2812"]{RFC2812}, ◊l["https://datatracker.ietf.org/doc/html/rfc2813"]{RFC2813}}
+		◊li{◊l["https://modern.ircdocs.horse/"]{The ◊e{modern} docs}}
+	}
 }
